6003621beb98ecd5a179591ffc3429057b612881
SeaCuts <125990960+SeaCutter@users.noreply.github.com>
2023-05-14 17:11:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -79,79 +79,6 @@ function addItemToCart(itemElement) {
     updateCart();
 }
 
-/* function removeItemFromCart(itemElement) {
-    const name = itemElement.querySelector('h3').textContent;
-
-    for (let i = 0; i < cart.length; i++) {
-        if (cart[i].name === name) {
-            cart[i].count--;
-            if (cart[i].count === 0) {
-                cart.splice(i, 1);
-            }
-            break;
-        }
-    }
-
-    updateCart();
-}
-*/
-
-/*function removeItemFromCart(itemElement) {
-  const name = itemElement.querySelector('h3').textContent;
-
-  const itemList = document.querySelectorAll('#fish-list li');
-  for (let i = 0; i < itemList.length; i++) {
-    const itemName = itemList[i].querySelector('h3').textContent;
-    if (itemName === name) {
-      for (let j = 0; j < cart.length; j++) {
-        if (cart[j].name === name) {
-          cart[j].count--;
-          if (cart[j].count === 0) {
-            cart.splice(j, 1);
-          }
-          break;
-        }
-      }
-      break;
-    }
-  }
-
-  updateCart();
-}
-*/
-/*
-function removeItemFromCart(itemElement) {
-  const name = itemElement.querySelector('h3').textContent;
-
-  for (let i = 0; i < cart.length; i++) {
-    if (cart[i].name === name) {
-      cart[i].count--;
-      if (cart[i].count === 0) {
-        cart.splice(i, 1);
-      }
-      break;
-    }
-  }
-
-  updateCart();
-
-  // update the count and disabled state of the buttons for this item
-  const items = document.querySelectorAll('li');
-  for (const item of items) {
-    if (item.querySelector('h3').textContent === name) {
-      const count = item.querySelector('.count');
-      const minusButton = item.querySelector('.minus-button');
-      if (count.textContent === '0') {
-        minusButton.setAttribute('disabled', true);
-      } else {
-        minusButton.removeAttribute('disabled');
-      }
-      count.textContent = cart.find(item => item.name === name)?.count || 0;
-    }
-  }
-}
-
-*/
 function removeItemFromCart(itemElement) {
   const name = itemElement.querySelector('h3').textContent;
 
@@ -172,88 +99,7 @@ function removeItemFromCart(itemElement) {
   // Update the cart display
   updateCart();
 }
-
-
 /*
-function updateCart() {
-  cartItems.innerHTML = '';
-
-  if (cart.length === 0) {
-    const emptyMessage = document.createElement('p');
-    emptyMessage.textContent = 'Your cart is empty';
-    cartItems.appendChild(emptyMessage);
-    return;
-  }
-
-  let totalPrice = 0;
-
-  for (let i = 0; i < cart.length; i++) {
-    const item = document.createElement('li');
-    const itemInfo = document.createElement('div');
-    const itemName = document.createElement('h3');
-    const itemCount = document.createElement('span');
-    const itemPrice = document.createElement('span');
-
-    item.classList.add('cart-item');
-    itemName.textContent = cart[i].name;
-    itemCount.textContent = ` x${cart[i].count}`;
-    itemPrice.textContent = cart[i].price;
-    totalPrice += cart[i].count * cart[i].price;
-
-    itemInfo.appendChild(itemName);
-    itemInfo.appendChild(itemCount);
-    itemInfo.appendChild(itemPrice);
-    item.appendChild(itemInfo);
-    cartItems.appendChild(item);
-  }
-
-  const clearCartButton = document.createElement('button');
-  clearCartButton.textContent = 'Clear Cart';
-  clearCartButton.addEventListener('click', () => {
-  // upto cart=[]; the code is from removeCartItem
-  const name = itemElement.querySelector('h3').textContent;
-
-  // Find the item in the cart
-  const itemIndex = cart.findIndex(item => item.name === name);
-  if (itemIndex === -1) {
-    // Item not found in cart
-    return;
-  }
-
-  // Decrement the item count or remove the item if count becomes zero
-  const item = cart[itemIndex];
-  item.count = 0;
-  if (item.count === 0) {
-    cart.splice(itemIndex, 1);
-  }
-
-  // Update the cart display
-  updateCart();
-
-  
-  
-    //cart = [];
-    //updateCart();
-  });
-
-  const totalPriceMessage = document.createElement('p');
-  totalPriceMessage.textContent = `Total: $${totalPrice.toFixed(2)}`;
-
-  cartItems.appendChild(clearCartButton);
-  cartItems.appendChild(totalPriceMessage);
-}
-
-
-const addButtons = document.querySelectorAll('.add-to-cart-button');
-addButtons.forEach((button) => {
-    const name = button.getAttribute('data-name');
-    button.addEventListener('click', () => {
-        const itemElement = button.parentElement.parentElement;
-        addItemToCart(itemElement);
-    });
-});
-
-*/
 function updateCart() {
 	const cartItems = document.querySelector('#cart-items');
    	cartItems.innerHTML = '';
@@ -310,6 +156,59 @@ addButtons.forEach((button) => {
     updateCart();
   });
 });
+*/
+function updateCart() {
+  const cartItems = document.querySelector('#cart-items');
+  cartItems.innerHTML = '';
+
+  if (cart.length === 0) {
+    const emptyMessage = document.createElement('p');
+    emptyMessage.textContent = 'Your cart is empty';
+    cartItems.appendChild(emptyMessage);
+    return;
+  }
+
+  let totalPrice = 0;
+
+  for (let i = 0; i < cart.length; i++) {
+    const item = document.createElement('li');
+    const itemInfo = document.createElement('div');
+    const itemName = document.createElement('h3');
+    const itemCount = document.createElement('span');
+    const itemPrice = document.createElement('span');
+
+    item.classList.add('cart-item');
+    itemName.textContent = cart[i].name;
+    itemCount.textContent = ` x${cart[i].count}`;
+    itemPrice.textContent = cart[i].price;
+    totalPrice += cart[i].count * cart[i].price;
+
+    itemInfo.appendChild(itemName);
+    itemInfo.appendChild(itemCount);
+    itemInfo.appendChild(itemPrice);
+    item.appendChild(itemInfo);
+    cartItems.appendChild(item);
+
+    // Update the count displayed for the item in the HTML
+    const itemElement = document.querySelector(`#fish-list li:nth-child(${i + 1})`);
+    const countSpan = itemElement.querySelector('.count');
+    countSpan.textContent = cart[i].count;
+  }
+
+  const clearCartButton = document.createElement('button');
+  clearCartButton.textContent = 'Clear Cart';
+  clearCartButton.addEventListener('click', () => {
+    cart = [];
+    updateCart();
+  });
+
+  const totalPriceMessage = document.createElement('p');
+  totalPriceMessage.textContent = `Total: $${totalPrice.toFixed(2)}`;
+
+  cartItems.appendChild(clearCartButton);
+  cartItems.appendChild(totalPriceMessage);
+}
+
 
 buyNowButton.addEventListener('click', (event) => {
 	event.preventDefault();
